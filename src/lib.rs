68e03e787581b33fa3ccb1d34f97bb76df8db296
Denis Deniz González <denis2.glez@gmail.com>
2021-12-30 14:45:54 +0100

    Add readme and license files.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod tests {
     use super::*;
     use std::{
         ffi::{CStr, CString},
+        fs::File,
         ptr,
     };
 
@@ -21,10 +22,9 @@ mod tests {
 
             let c_str = CString::new("data/temp.tar").unwrap();
             let mut r = archive_read_open_filename(a, c_str.as_ptr() as *const i8, 10240) as u32;
-            // Note 1
 
             if r != ARCHIVE_OK {
-                panic!("Fails on archive_read_open_filename!");
+                panic!("archive_read_open_filename fails!");
             }
 
             let mut entry: *mut archive_entry = ptr::null_mut();
@@ -38,10 +38,63 @@ mod tests {
                 archive_read_data_skip(a);
             }
 
-            r = archive_read_free(a) as u32; // Note 3
+            r = archive_read_free(a) as u32;
             if r != ARCHIVE_OK {
-                panic!("Fails on archive_read_free!");
+                panic!("archive_read_free fails!");
             }
         }
     }
+
+    // #[test]
+    // fn list_in_memory_archive_content() {
+    //     unsafe {
+    //         let a = archive_read_new();
+    //         archive_read_support_compression_gzip(a);
+    //         archive_read_support_format_tar(a);
+    //     }
+    // }
+
+    // #[repr(C)]
+    // struct mydata {
+    //     name: String,
+    //     fd: File,
+    // }
+
+    // #[test]
+    // fn list_archive_content_custom_function() {
+    //     let name = "hello";
+    //     let mydata = mydata {
+    //         name: name.to_owned(),
+    //         fd: File::open(name).unwrap(),
+    //     };
+
+    //     unsafe {
+    //         let a = archive_read_new();
+    //         archive_read_support_compression_all(a);
+    //         archive_read_support_format_all(a);
+
+    //         // let c_str = CString::new("data/temp.tar").unwrap();
+    //         let mut r = archive_read_open(a, mydata, ptr::null(), myread, myclose) as u32;
+
+    //         if r != ARCHIVE_OK {
+    //             panic!("archive_read_open_filename fails!");
+    //         }
+
+    //         let mut entry: *mut archive_entry = ptr::null_mut();
+    //         while archive_read_next_header(a, &mut entry) == ARCHIVE_OK as i32 {
+    //             println!(
+    //                 "- {}",
+    //                 CStr::from_ptr(archive_entry_pathname(entry))
+    //                     .to_str()
+    //                     .unwrap()
+    //             );
+    //             archive_read_data_skip(a);
+    //         }
+
+    //         r = archive_read_free(a) as u32;
+    //         if r != ARCHIVE_OK {
+    //             panic!("archive_read_free fails!");
+    //         }
+    //     }
+    // }
 }
