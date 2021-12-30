701238ac07480dfb1dc42aceb367b2ba449bb6dd
Denis Deniz González <denis2.glez@gmail.com>
2021-12-30 10:23:31 +0100

    Starting with a minimal `bindgen` setup.

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        ffi::{CStr, CString},
+        ptr,
+    };
+
+    #[test]
+    fn it_works() {
+        unsafe {
+            let a = archive_read_new();
+            archive_read_support_filter_all(a);
+            archive_read_support_format_all(a);
+
+            let c_str = CString::new("data/temp.tar").unwrap();
+            let r = archive_read_open_filename(a, c_str.as_ptr() as *const i8, 10240) as u32;
+            // Note 1
+
+            if r != ARCHIVE_OK {
+                panic!("Fails on archive_read_open_filename!");
+            }
+
+            let mut entry: *mut archive_entry = ptr::null_mut();
+            while archive_read_next_header(a, &mut entry) == ARCHIVE_OK as i32 {
+                println!(
+                    "- {}",
+                    CStr::from_ptr(archive_entry_pathname(entry))
+                        .to_str()
+                        .unwrap()
+                );
+                archive_read_data_skip(a);
+            }
+        }
+    }
+}
